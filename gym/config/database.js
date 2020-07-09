58233e1de525171e59c709fa6f2a172034e06e1b
atul kumar <atul.11192@gmail.com>
2020-07-09 16:05:23 +0530

    making changes two more projects

---
+++ gym/config/database.js
@@ -0,0 +1,19 @@
+const mysql=require('mysql');
+const db=mysql.createConnection({
+    user:'root',
+    password:'seed',
+    database:'gymms',
+    host:'localhost',
+    port:'3306',
+    multipleStatements:true
+});
+db.connect((err)=>{
+    if(err)
+    {
+      console.log('database connected');
+    }
+    else
+    {
+       console.log('error'+err);
+    }
+});
