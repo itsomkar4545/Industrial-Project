2e49f9494a7679c2206bd488094d3498a9745317
atul kumar <atul.11192@gmail.com>
2020-07-14 22:04:49 +0530

    adding more views

---
+++ onlinemoviebooking/config/database.js
@@ -0,0 +1,20 @@
+const mysql=require('mysql');
+const db=mysql.createConnection({
+    database:'movietiming',
+    host:'localhost',
+    port:'3306',
+    user:'root',
+    password:'seed',
+    multipleStatements:true
+});
+db.connect((err,result)=>{
+    if(!err)
+    {
+        console.log('databse connected');
+    }
+    else
+    {
+        console.log('error '+err);
+    }
+});
+module.exports=db;
